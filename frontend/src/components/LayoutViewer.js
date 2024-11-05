50de3caa1f541db6eff3a305783ddc15986dd7b0
geeth eshan <98637377+GeethEshan@users.noreply.github.com>
2024-11-05 21:54:40 +0530

    Update LayoutViewer.js

--- frontend/src/components/LayoutViewer.js
+++ frontend/src/components/LayoutViewer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useLocation, useNavigate } from "react-router-dom";
 import { QRCodeCanvas } from "qrcode.react";
